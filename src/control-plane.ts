aebeb79c2173d8cf9e7114e02e4761e428a8d13e
Viktor Malyshev <vmalyshev0@gmail.com>
2023-09-22 13:23:27 +0200

    control plane, draft version

---
+++ src/control-plane.ts
@@ -0,0 +1,42 @@
+// Class is needed as there is no way to pass things around
+// or create more than one plane for this project
+import {Elevator, ElevatorRequest, RequestStatus} from './interfaces.js'
+import {ElevatorEngine} from './engine.js'
+
+export class ControlPlane {
+
+    public readonly floors: number
+    public readonly elevators: Elevator[]
+    private _queue: ElevatorRequest[] = []
+    private readonly engine: ElevatorEngine
+
+    constructor(floors: number, elevators: Elevator[]) {
+        this.floors = floors
+        this.elevators = elevators
+        this.engine = new ElevatorEngine(this)
+    }
+
+    hasRequests(): boolean {
+        return this._queue.length > 0
+    }
+
+    addRequest(request: ElevatorRequest): void {
+        this._queue.push(request)
+    }
+
+    removeRequest(request: ElevatorRequest): ElevatorRequest[] {
+        const index = this._queue.indexOf(request)
+
+        if (index === -1) throw new Error('Request is not found')
+
+        return this._queue.splice(index, 1)
+    }
+
+    getNextRequest(): ElevatorRequest | undefined {
+        return this._queue.find(e => e.status === RequestStatus.INIT)
+    }
+
+    startEngine(): void {
+        this.engine.startEngine()
+    }
+}
