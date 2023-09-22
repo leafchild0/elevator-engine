4cdf812a4fbbcc310a14ba6876acc7f955462a62
Viktor Malyshev <vmalyshev0@gmail.com>
2023-09-22 13:23:17 +0200

    added link to request and name, added status for request

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,16 +1,30 @@
 export interface Elevator {
+    name: string,
     floor: number,
     direction: Direction,
-    weight: number,
-    enabled: boolean
+    // If there is a request elevator is trying to fulfill
+    request?: ElevatorRequest
+    // Should probably be added later to decide which elevator
+    // will come if other conditions are the same
+    //weight: number,
+    // Will be added in the next versions for simplicity
+    //enabled: boolean
 }
 
 export enum Direction {
-    UP,
-    DOWN
+    UP = 'Up',
+    DOWN = 'Down'
+}
+
+export enum RequestStatus {
+    INIT= 'Init',
+    PENDING = 'Pending',
+    IN_PROGRESS = 'In Progress'
 }
 
 export interface ElevatorRequest {
-    floor: number,
-    direction: Direction
+    desired: number,
+    start: number,
+    direction: Direction,
+    status: RequestStatus
 }
